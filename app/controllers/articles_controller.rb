879a87b2f3ec4e3a6a669302f914f4c11d0894ee
codeshootah <grigorycode@gmail.com>
2019-08-03 15:31:59 +0300

    Form partial, destroy action and dry controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -26,17 +26,25 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-  
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
+
+  def destroy
+    @article.destroy
+  
+    redirect_to articles_path
+  end
   
   private
     def article_params
       params.require(:article).permit(:title, :text)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
     end
 end
