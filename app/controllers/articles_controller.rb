5ea92494c242f09e8f1df2caaac38a69f58dadf7
codeshootah <grigorycode@gmail.com>
2019-08-15 02:39:03 +0300

    Associations between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,14 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new
+    @article = current_user.article.build
   end
 
   def edit
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.article.build(article_params)
   
     if @article.save
       redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_223605) do
+ActiveRecord::Schema.define(version: 2019_08_14_230829) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
